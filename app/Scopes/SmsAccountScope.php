fcfc972f4aaa8da6f1f91ed7a47cae2f47720e95
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2021-08-16 21:43:18 +0000

    SMS account scope bug fixes

--- app/Scopes/SmsAccountScope.php
+++ app/Scopes/SmsAccountScope.php
@@ -5,12 +5,13 @@ namespace App\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use NunoMazer\Samehouse\Facades\Landlord;
 
 class SmsAccountScope implements Scope {
 
     public function apply(Builder $builder, Model $model)
     {
-        $organisation_id = request('organisation_id');
+        $organisation_id = Landlord::getTenants()->values()->first();
 
         $builder->join('module_sms_accounts', function($join) use($model, $organisation_id) {
             $join->on('module_sms_accounts.id', '=', "{$model->getTable()}.module_sms_account_id")
