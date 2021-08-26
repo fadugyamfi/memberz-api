243520c13763fdadeb345ef14c04862373c653b1
Derrick Korku <derrickkorku1@gmail.com>
2021-08-26 15:50:13 +0000

    Auto generate member contribution receipt

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\MemberContribution;
 use App\Models\Organisation;
 use App\Models\OrganisationAccount;
 use App\Models\OrganisationFileImport;
@@ -12,6 +13,7 @@ use App\Models\OrganisationMemberCategory;
 use App\Models\OrganisationSubscription;
 use App\Models\SmsAccountMessage;
 use App\Models\SmsBroadcast;
+use App\Observers\MemberContributionObserver;
 use App\Observers\OrganisationAccountObserver;
 use App\Observers\OrganisationFileImportObserver;
 use App\Observers\OrganisationInvoiceItemObserver;
@@ -58,5 +60,6 @@ class EventServiceProvider extends ServiceProvider
         SmsAccountMessage::observe(SmsAccountMessageObserver::class);
         SmsBroadcast::observe(SmsBroadcastObserver::class);
         OrganisationFileImport::observe(OrganisationFileImportObserver::class);
+        MemberContribution::observe(MemberContributionObserver::class);
     }
 }
