df1c6893381fc0b46c75ebad1e3d11693d09e4a9
Derrick Korku <derrickkorku1@gmail.com>
2021-09-27 20:31:06 +0000

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('activitylog:clean')->monthly();
+        $schedule->command('activitylog:clean')->quarterly();
     }
 
     /**
