65bf7af8b64eea139940b2145a44f9d87a31b020
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2021-11-24 15:05:10 +0000

    Code adjustments

--- app/Http/Controllers/Reporting/MonthlyConsolidatedReportController.php
+++ app/Http/Controllers/Reporting/MonthlyConsolidatedReportController.php
@@ -13,16 +13,23 @@ class MonthlyConsolidatedReportController
 
     public function __invoke(MonthlyConsolidatedReportRequest $request)
     {
-        $byContributionTypes = ContributionSummary::getByCurrencyId($request->currency_id)->getByYear($request->year)->with('contributionType')->selectRaw('month, module_contribution_type_id, sum(amount) as amount')
-            ->groupBy('month')->groupBy('module_contribution_type_id')->orderBy('month')->orderBy('module_contribution_type_id')
-            ->get()->transform(function ($d) {
-            return [
-                'month' => $d->month,
-                'amount' => $d->amount,
-                'contribution_type_id' => $d->module_contribution_type_id,
-                'contribution_type_name' => optional($d->contributionType)->name,
-            ];
-        });
+        $byContributionTypes = ContributionSummary::getByCurrencyId($request->currency_id)
+            ->getByYear($request->year)
+            ->with(['contributionType', 'currency'])
+            ->selectRaw('month, currency_id, module_contribution_type_id, sum(amount) as amount')
+            ->groupBy('month')->groupBy('module_contribution_type_id')->groupBy('currency_id')
+            ->orderBy('month')->orderBy('module_contribution_type_id')
+            ->get()
+            ->transform(function ($d) {
+                return [
+                    'month' => $d->month,
+                    'amount' => $d->amount,
+                    'contribution_type_id' => $d->module_contribution_type_id,
+                    'contribution_type_name' => optional($d->contributionType)->name,
+                    'currency_id' => $d->currency_id,
+                    'currency_code' => $d->currency->currency_code
+                ];
+            });
 
         $byPaymentTypes = Contribution::byYear($request->year)->byCurrencyId($request->country_id)->with('contributionPaymentType')->selectRaw('month, module_contribution_payment_type_id, sum(amount) as amount')
             ->groupBy('month')->groupBy('module_contribution_payment_type_id')->orderBy('month')->orderBy('module_contribution_payment_type_id')
