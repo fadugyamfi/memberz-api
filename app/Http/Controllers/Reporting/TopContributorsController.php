240805327f033fc7dfefc7ce79c4bf64beae3dbc
Derrick Korku <derrickkorku1@gmail.com>
2021-11-29 15:09:37 +0000

    Updated top contributors report

--- app/Http/Controllers/Reporting/TopContributorsController.php
+++ app/Http/Controllers/Reporting/TopContributorsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Reporting;
 
 use App\Http\Requests\TopContributorsRequest;
+use App\Models\Contribution;
 
 /**
  * @group Finance Reporting
@@ -12,6 +13,24 @@ class TopContributorsController
 
     public function __invoke(TopContributorsRequest $request)
     {
-        return [];
+        return Contribution::byYear($request->year)->byCurrencyId($request->currency_id)->whereNotNull('organisation_member_id')
+            ->with(['organisationMember.member', 'currency'])
+            ->selectRaw('currency_id, year, organisation_member_id, sum(amount) as total_amount, avg(amount) as avg_amount')
+            ->groupBy('organisation_member_id')->groupBy('currency_id')->groupBy('year')
+            ->orderBy('total_amount', 'desc')->orderBy('organisation_member_id')->orderBy('currency_id')->orderBy('year')
+            ->limit(50)->get()
+            ->transform(function ($d) {
+                $org = $d->organisationMember;
+                return [
+                    'currency_code' => optional($d->currency)->currency_code,
+                    'total_amount' => $d->total_amount,
+                    'avg_amount' => $d->avg_amount,
+                    'year' => $d->year,
+                    'member_name' => $org ? $org->member->name : '',
+                    'mobile_number' => $org ? $org->member->mobile_number : '',
+                    'age' => $org ? $org->member->age : '',
+                ];
+            });
+
     }
 }
