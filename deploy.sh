9a121acdc6b9a6561cfa1d27b23eb4424ff24b6b
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2022-01-18 11:47:34 +0000

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 git pull
 composer install --prefer-dist --no-dev -o
 composer dump-autoload
-php artisan config:clear && php artisan cache:clear && php artisan route:clear
+php artisan cache:clear && php artisan config:cache &&  && php artisan route:cache
 php artisan storage:link
 php artisan migrate --force
 php artisan queue:restart
