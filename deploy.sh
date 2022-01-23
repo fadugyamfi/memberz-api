b42d845d28c59a23259008ab6bc13c43ff1967b6
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2022-01-23 20:34:46 +0000

    Changing JWT Auth Library

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git pull
-composer install --prefer-dist --no-dev -o
+composer install --prefer-dist --no-dev -o --ignore-platform-reqs
 composer dump-autoload
 php artisan cache:clear && php artisan config:cache &&  && php artisan route:cache
 php artisan storage:link
