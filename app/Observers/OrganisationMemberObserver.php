39ac9160b27e8f4053e6ef544f933bdd42921fa9
Derrick Korku <derrickkorku1@gmail.com>
2022-02-19 15:55:11 +0000

    Send E-Mail notification registration form completed successfully and on member registration status updated.

--- app/Observers/OrganisationMemberObserver.php
+++ app/Observers/OrganisationMemberObserver.php
@@ -2,8 +2,11 @@
 
 namespace App\Observers;
 
+use App\Mail\MemberFormRegistrationCompleted;
+use App\Mail\MemberRegistrationStatusUpdated;
 use App\Models\OrganisationMember;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class OrganisationMemberObserver
 {
@@ -47,7 +50,12 @@ class OrganisationMemberObserver
         if( $organisationMember->isDirty('organisation_no') ) {
             $this->incrementMembershipIDCounter($organisationMember);
         }
+
+        if( $organisationMember->source == 'registration' && $organisationMember->member->email ) {
+            Mail::to($organisationMember->member->email)->send(new MemberFormRegistrationCompleted());
+        }
     }
+    
 
     /**
      * Handle the organisation member "updating" event.
@@ -63,6 +71,20 @@ class OrganisationMemberObserver
             // we update the counter immediately after auto generating the number in the case of an update
             // since we can't determine if an update to the organisation_no was manual or auto generated.
             $this->incrementMembershipIDCounter($organisationMember);
+        }
+    }
+
+
+    /**
+     * Handle the organisation member "updated" event.
+     *
+     * @param  \App\Models\OrganisationMember  $organisationMember
+     * @return void
+     */
+    public function updated(OrganisationMember $organisationMember)
+    {
+        if( $organisationMember->isDirty('approved') && $organisationMember->isDirty('active') && $organisationMember->member->email ) {
+            Mail::to($organisationMember->member->email)->send(new MemberRegistrationStatusUpdated($organisationMember));
         }
     }
 
