6ba1bb3f37ad9bb88ef55a59b8655bf93afc7b13
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2022-06-20 22:30:03 +0000

    Code re-org

--- app/Console/Commands/ScheduleBirthdayMessages.php
+++ app/Console/Commands/ScheduleBirthdayMessages.php
@@ -2,11 +2,11 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\OrganisationMember;
 use App\Models\OrganisationSetting;
 use App\Models\SmsAccount;
 use App\Models\SmsAccountMessage;
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use NunoMazer\Samehouse\Facades\Landlord;
 
@@ -47,48 +47,51 @@ class ScheduleBirthdayMessages extends Command
             ->pluck('organisation_id')
             ->all();
 
-        foreach($organisation_ids as $organisation_id) {
+        foreach ($organisation_ids as $organisation_id) {
             Landlord::addTenant('organisation_id', $organisation_id);
             Landlord::applyTenantScopesToDeferredModels();
 
-            $smsAccount = SmsAccount::activeAccount($organisation_id)->first();
-            $message = OrganisationSetting::getAutomatedBirthdayMessage($organisation_id);
-            $messageTime = OrganisationSetting::getAutomatedBirthdayMessageSendTime($organisation_id);
+            $this->messageBirthdayCelebrants($organisation_id);
+        }
+    }
 
-            $membersBirthdayToday = OrganisationMember::birthdayCelebrants($organisation_id)->get();
+    public function messageBirthdayCelebrants($organisation_id)
+    {
+        $smsAccount = SmsAccount::activeAccount($organisation_id)->first();
+        $message = OrganisationSetting::getAutomatedBirthdayMessage($organisation_id);
+        $messageTime = OrganisationSetting::getAutomatedBirthdayMessageSendTime($organisation_id);
 
+        $membersBirthdayToday = OrganisationMember::birthdayCelebrants($organisation_id)->get();
 
-            $membersBirthdayToday->each(function($member) use ($smsAccount, $message, $messageTime) {
-                if( !$member->mobile_number || strlen($member->mobile_number) < 9  ) {
-                    Log::info("Birthday Message: Skipping Empty/Invalid Phone number: {$smsAccount->organisation_id}: {$member->first_name} {$member->last_name}, {$member->mobile_number}");
-                    return;
-                }
+        $membersBirthdayToday->each(function ($member) use ($smsAccount, $message, $messageTime) {
+            if (!$member->mobile_number || strlen($member->mobile_number) < 9) {
+                Log::info("Birthday Message: Skipping Empty/Invalid Phone number: {$smsAccount->organisation_id}: {$member->first_name} {$member->last_name}, {$member->mobile_number}");
+                return;
+            }
 
-                $messagedToday = SmsAccountMessage::whereDate('module_sms_account_instant_messages.created', now()->format('Y-m-d'))
-                    ->where([
-                        'module_sms_account_id' => $smsAccount->id,
-                        'member_id' => $member->id,
-                        'bday_msg' => true
-                    ])
-                    ->first();
-
-                if( $messagedToday ) {
-                    Log::info("Birthday Message: Already sent/scheduled: {$smsAccount->organisation_id}: {$member->first_name} {$member->last_name}, {$member->mobile_number}");
-                    return;
-                }
-
-                // create an instant message which will be scheduled for sending in the SmsAccountMessageObserver
-                SmsAccountMessage::create([
+            $messagedToday = SmsAccountMessage::whereDate('module_sms_account_instant_messages.created', now()->format('Y-m-d'))
+                ->where([
                     'module_sms_account_id' => $smsAccount->id,
                     'member_id' => $member->id,
-                    'to' => $member->mobile_number,
-                    'message' => $message,
-                    'sender_id' => $smsAccount->sender_id,
                     'bday_msg' => true,
-                    'sent_at' => now()->format('Y-m-d') . " ${messageTime}:00"
-                ]);
-            });
-        }
+                ])
+                ->first();
 
+            if ($messagedToday) {
+                Log::info("Birthday Message: Already sent/scheduled: {$smsAccount->organisation_id}: {$member->first_name} {$member->last_name}, {$member->mobile_number}");
+                return;
+            }
+
+            // create an instant message which will be scheduled for sending in the SmsAccountMessageObserver
+            SmsAccountMessage::create([
+                'module_sms_account_id' => $smsAccount->id,
+                'member_id' => $member->id,
+                'to' => $member->mobile_number,
+                'message' => $message,
+                'sender_id' => $smsAccount->sender_id,
+                'bday_msg' => true,
+                'sent_at' => now()->format('Y-m-d') . " ${messageTime}:00",
+            ]);
+        });
     }
 }
